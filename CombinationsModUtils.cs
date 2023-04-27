0e192f39eae14be6d75f35e012a7e1163b4b3548
Moo Moo <obsidian1392@gmail.com>
2023-04-27 11:10:49 -0600

    Created Utils

---
+++ CombinationsModUtils.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombinationsMod
+{
+    public class CombinationsModUtils
+    {
+        public static Color MulticolorLerp(float increment, params Color[] colors)
+        {
+            increment %= 0.999f;
+            int num = (int)(increment * (float)colors.Length);
+            Color value = colors[num];
+            Color value2 = colors[(num + 1) % colors.Length];
+            return Color.Lerp(value, value2, increment * (float)colors.Length % 1f);
+        }
+    }
+}

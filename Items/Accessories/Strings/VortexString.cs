3906a4bc13f9b70b5c24dd8f5526d264ac318b1c
Moo Moo <obsidian1392@gmail.com>
2023-05-02 08:18:44 -0600

    Added 3 new strings

---
+++ Items/Accessories/Strings/VortexString.cs
@@ -0,0 +1,54 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.GameContent;
+using Terraria.ID;
+using CombinationsMod.Rarities;
+using CombinationsMod.Items.Bars;
+using CombinationsMod.Items.Souls;
+using CombinationsMod.UI;
+
+namespace CombinationsMod.Items.Accessories.Strings
+{
+    public class VortexString : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Vortex String");
+            Tooltip.SetDefault("Increases yoyo range\n[c/6EAE6E:+250 yoyo range]\n[c/6EAE6E:+5% base yoyo damage]");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 28;
+            Item.height = 28;
+            Item.rare = ModContent.RarityType<DevBagRarity>();
+            Item.accessory = true;
+            Item.maxStack = 1;
+            Item.value = Item.sellPrice(gold: 5);
+            Item.stringColor = 37;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            YoyoModPlayer modPlayer = player.GetModPlayer<YoyoModPlayer>();
+            if (!hideVisual)
+            {
+                modPlayer.vortexString = true;
+                player.yoyoString = true;
+            }
+
+        }
+        public override bool CanEquipAccessory(Player player, int slot, bool modded)
+        {
+            return modded && (LoaderManager.Get<AccessorySlotLoader>().Get(slot, player).Type == ModContent.GetInstance<StringSlot>().Type);
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.FragmentStardust, 20)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+        }
+    }
+}

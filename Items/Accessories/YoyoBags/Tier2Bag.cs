00c2e7a90735af6c9d5e2b75463d6a840637ea70
Moo Moo <obsidian1392@gmail.com>
2023-05-26 17:58:33 -0600

    New yoyo bags

--- Items/Accessories/YoyoBags/Tier2Bag.cs
+++ Items/Accessories/YoyoBags/Tier2Bag.cs
@@ -2,7 +2,6 @@ using CombinationsMod.UI;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
-using System.Linq;
 using Terraria;
 using Terraria.GameContent;
 using Terraria.ID;
@@ -10,18 +9,17 @@ using Terraria.ModLoader;
 
 namespace CombinationsMod.Items.Accessories.YoyoBags
 {
-    public class ShimmerBag : ItemLoader
+    public class Tier2Bag : ItemLoader
     {
         public override void SetDefaults()
         {
-            Item.width = 52;
-            Item.height = 46;
-            Item.rare = ItemRarityID.Pink;
+            Item.width = 36;
+            Item.height = 44;
+            Item.rare = ItemRarityID.LightPurple;
             Item.accessory = true;
             Item.maxStack = 1;
             Item.value = Item.sellPrice(gold: 12);
-
-            ItemID.Sets.ShimmerTransformToItem[Type] = ModContent.ItemType<Tier2Bag>();
+            ItemID.Sets.ShimmerTransformToItem[Type] = ModContent.ItemType<ShimmerBag>();
         }
 
         public override void UpdateAccessory(Player player, bool hideVisual)
@@ -29,7 +27,8 @@ namespace CombinationsMod.Items.Accessories.YoyoBags
             YoyoModPlayer modPlayer = player.GetModPlayer<YoyoModPlayer>();
             if (!hideVisual)
             {
-                modPlayer.shimmerBag = true;
+                modPlayer.tier2Bag = true;
+
                 if (!ModContent.GetInstance<YoyoModConfig>().EnableModifiedYoyoBag)
                 {
                     player.yoyoGlove = true;
@@ -48,9 +47,9 @@ namespace CombinationsMod.Items.Accessories.YoyoBags
 
         public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
         {
-            Texture2D tex = ModContent.Request<Texture2D>("CombinationsMod/Items/Accessories/YoyoBags/ShimmerBag").Value;
+            Texture2D tex = ModContent.Request<Texture2D>("CombinationsMod/Items/Accessories/YoyoBags/Tier2Bag").Value;
 
-            Main.spriteBatch.Draw(tex, position, null, drawColor, 0, origin, scale * 1.18f, SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(tex, position, null, drawColor, 0, origin, scale * 1.13f, SpriteEffects.None, 0f);
             return false;
         }
 
@@ -58,12 +57,21 @@ namespace CombinationsMod.Items.Accessories.YoyoBags
         {
             if (ModContent.GetInstance<YoyoModConfig>().EnableModifiedYoyoBag)
             {
-                tooltips.Add(new TooltipLine(Mod, "BagInfo", "Yoyos are recalled faster\nGives the user more accessory slots for yoyos\nAllows the use of Support Gloves and Yoyo Drills\nYoyos can phase through blocks"));
+                tooltips.Add(new TooltipLine(Mod, "BagInfo", "Yoyos are recalled faster\nGives the user more accessory slots for yoyos\nAllows the use of Support Gloves and Yoyo Drills\n\"May or may not be shimmerable\""));
             }
             else
             {
-                tooltips.Add(new TooltipLine(Mod, "BagInfo2", "Gives the user master yoyo skills\nAdds one additional yoyo\nYoyos can phase through blocks"));
+                tooltips.Add(new TooltipLine(Mod, "BagInfo2", "Gives the user master yoyo skills\nAdds one additional yoyo\n\"May or may not be shimmerable\""));
             }
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.YoyoBag)
+                .AddIngredient(ItemID.ChlorophyteBar, 15)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
         }
     }
 }   

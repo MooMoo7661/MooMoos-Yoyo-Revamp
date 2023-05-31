2cdb8adcba8944a644f188b24751b70a6f83e836
Moo Moo <obsidian1392@gmail.com>
2023-05-30 20:46:57 -0600

    Rings can now have prefixes

--- Items/Accessories/Rings/TerreneRing.cs
+++ Items/Accessories/Rings/TerreneRing.cs
@@ -26,10 +26,7 @@ namespace CombinationsMod.Items.Accessories.Rings
             Item.maxStack = 1;
             Item.value = Item.sellPrice(0, 30, 5, 0);
         }
-        public override bool? PrefixChance(int pre, Terraria.Utilities.UnifiedRandom rand)
-        {
-            return false;
-        }
+
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
             YoyoModPlayer modPlayer = player.GetModPlayer<YoyoModPlayer>();

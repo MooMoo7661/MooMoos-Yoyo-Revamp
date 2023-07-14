e59fdc38392f5762782482d7adb738b64fe5910b
Moo Moo <obsidian1392@gmail.com>
2023-07-14 08:57:30 -0600

    Swirls no longer use local i-frames

--- Projectiles/YoyoEffects/BaseSwirl.cs
+++ Projectiles/YoyoEffects/BaseSwirl.cs
@@ -49,9 +49,6 @@ namespace CombinationsMod.Projectiles.YoyoEffects
             Projectile.penetrate = Penetrate;
             Projectile.extraUpdates = 1;
             Projectile.timeLeft = 150;
-            Projectile.usesIDStaticNPCImmunity = false;
-            Projectile.usesLocalNPCImmunity = true;
-            Projectile.localNPCHitCooldown = 10 * Projectile.MaxUpdates;
         }
 
         public override Color? GetAlpha(Color lightColor)
--- Projectiles/YoyoEffects/Solid/CultistRing3.cs
+++ Projectiles/YoyoEffects/Solid/CultistRing3.cs
@@ -29,9 +29,6 @@ namespace CombinationsMod.Projectiles.YoyoEffects.Solid
             Projectile.penetrate = -1;
             Projectile.extraUpdates = 1;
             Projectile.timeLeft = 150;
-            Projectile.usesIDStaticNPCImmunity = false;
-            Projectile.usesLocalNPCImmunity = true;
-            Projectile.localNPCHitCooldown = 10 * Projectile.MaxUpdates;
         }
         public override string Texture => "CombinationsMod/Projectiles/YoyoEffects/ScalableHitbox1";
 

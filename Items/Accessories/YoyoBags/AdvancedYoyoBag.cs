d4af768dff540966e9594e8c4d360899e5bb1c83
Moo Moo <obsidian1392@gmail.com>
2023-07-14 20:00:46 -0600

    Added Alien Bag

---
+++ Items/Accessories/YoyoBags/AdvancedYoyoBag.cs
@@ -0,0 +1,52 @@
+using CombinationsMod.UI;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CombinationsMod.Items.Accessories.YoyoBags
+{
+    public class AdvancedYoyoBag : ItemLoader
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 56;
+            Item.height = 50;
+            Item.rare = ItemRarityID.Lime;
+            Item.accessory = true;
+            Item.maxStack = 1;
+            Item.value = Item.sellPrice(gold: 8, silver:39);
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            YoyoModPlayer modPlayer = player.GetModPlayer<YoyoModPlayer>();
+            modPlayer.alienBag = true;
+            if (!ModContent.GetInstance<YoyoModConfig>().EnableModifiedYoyoBag)
+            {
+                player.yoyoGlove = true;
+                player.yoyoString = true;
+            }
+        }
+
+        public override bool IsLoadingEnabled(Mod mod)
+        {
+            if (!ModContent.GetInstance<YoyoModConfig>().LoadModdedAccessories)
+                return false;
+
+            return ModContent.GetInstance<YoyoModConfig>().LoadModdedItems;
+        }
+
+        public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            Texture2D tex = ModContent.Request<Texture2D>("CombinationsMod/Items/Accessories/YoyoBags/AdvancedYoyoBag").Value;
+
+            Main.spriteBatch.Draw(tex, position, null, drawColor, 0, origin, scale * 1.18f, SpriteEffects.None, 0f);
+            return false;
+        }
+    }
+}   

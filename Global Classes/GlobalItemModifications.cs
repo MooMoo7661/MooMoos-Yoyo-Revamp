52242e4b7869fcb64e55246424c51708cbbca23e
Moo Moo <obsidian1392@gmail.com>
2023-09-25 10:38:10 -0600

    Removed unneeded tooltip additions

--- Global Classes/GlobalItemModifications.cs
+++ Global Classes/GlobalItemModifications.cs
@@ -91,63 +91,7 @@ namespace CombinationsMod.GlobalClasses
         {
             if (item.type >= ItemID.BlackCounterweight && item.type <= ItemID.YellowCounterweight) // Is a counterweight
             {
-                tooltips.Add(new TooltipLine(Mod, "CounterweightStackInfo", "[c/FFD7D7:Each Yoyo that hits a target will produce a counterweight]"));
-            }
-
-            if (Main.LocalPlayer.GetModPlayer<YoyoModPlayer>().yoyoRing)
-            {
-                switch (item.type)
-                {
-                    case ItemID.Cascade:
-                        tooltips.Insert(2, new TooltipLine(Mod, "Yoyo Ability", "[c/B3FDFF:Automatically triggers]"));
-                        tooltips.Insert(3, new TooltipLine(Mod, "Yoyo Ability Description", "[c/B3FDFF:Special Ability : Creates an explosion every 5 hits, which does 1.5x the yoyo damage]"));
-                        break;
-
-                    case ItemID.Valor:
-                        tooltips.Insert(2, new TooltipLine(Mod, "Yoyo Ability", "[c/B3FDFF:Automatically triggers]"));
-                        tooltips.Insert(3, new TooltipLine(Mod, "Yoyo Ability Description", "[c/B3FDFF:Special Ability : Creates homing water bolts that explode on contact, dealing area damage]"));
-                        break;
-
-                    case ItemID.CrimsonYoyo or ItemID.CorruptYoyo:
-                        tooltips.Insert(2, new TooltipLine(Mod, "Yoyo Ability", "[c/B3FDFF:Triggers on hit]"));
-                        tooltips.Insert(3, new TooltipLine(Mod, "Yoyo Ability Description", "[c/B3FDFF:Special Ability : Heals the user on hit]"));
-                        break;
-
-                    case ItemID.Rally:
-                        tooltips.Insert(2, new TooltipLine(Mod, "Yoyo Ability", "[c/B3FDFF:Triggers automatically]"));
-                        tooltips.Insert(3, new TooltipLine(Mod, "Yoyo Ability Description", "[c/B3FDFF:Special Ability : Creates 3 swirling copies that deal 1/2 of the yoyo's damage]"));
-                        break;
-
-                    case ItemID.Code1:
-                        tooltips.Insert(2, new TooltipLine(Mod, "Yoyo Ability", "[c/B3FDFF:Triggers automatically]"));
-                        tooltips.Insert(3, new TooltipLine(Mod, "Yoyo Ability Description", "[c/B3FDFF:Special Ability : Creates 6 swirling copies that deal 0.8x the yoyo damage]"));
-                        break;
-
-                    case ItemID.FormatC:
-                        tooltips.Insert(2, new TooltipLine(Mod, "Yoyo Ability", "[c/B3FDFF:Triggers automatically]"));
-                        tooltips.Insert(3, new TooltipLine(Mod, "Yoyo Ability Description", "[c/B3FDFF:Special Ability : Creates 4 swirling copies that deal 100% of the yoyo's damage]\n[c/B3FDFF:These copies will split apart and fly away after the yoyo has been recalled]"));
-                        break;
-
-                    case ItemID.HelFire:
-                        tooltips.Insert(2, new TooltipLine(Mod, "Yoyo Ability", "[c/B3FDFF:Triggers on hit]"));
-                        tooltips.Insert(3, new TooltipLine(Mod, "Yoyo Ability Description", "[c/B3FDFF:Special Ability : Inflicts the target with Combustion, dealing a large amount of fire damage]"));
-                        break;
-
-                    case ItemID.Amarok:
-                        tooltips.Insert(2, new TooltipLine(Mod, "Yoyo Ability", "[c/B3FDFF:Triggers automatically]"));
-                        tooltips.Insert(3, new TooltipLine(Mod, "Yoyo Ability Description", "[c/B3FDFF:Special Ability : Shoots out 8 ice spikes every second that deal 0.6x the yoyo's damage]"));
-                        break;
-
-                    case ItemID.Gradient:
-                        tooltips.Insert(2, new TooltipLine(Mod, "Yoyo Ability", "[c/B3FDFF:Triggers automatically]"));
-                        tooltips.Insert(3, new TooltipLine(Mod, "Yoyo Ability Description", "[c/B3FDFF:Special Ability : Creates 8 swirling copies that deal 100% of the yoyo damage]\n[c/B3FDFF:These copies will split apart and fly away after the yoyo has been recalled]\n[c/B3FDFF:When near an enemy, they will home in on them]"));
-                        break;
-
-                    case ItemID.TheEyeOfCthulhu:
-                        tooltips.Insert(2, new TooltipLine(Mod, "Yoyo Ability", "[c/B3FDFF:Triggers automatically]"));
-                        tooltips.Insert(3, new TooltipLine(Mod, "Yoyo Ability Description", "[c/B3FDFF:Special Ability : Has 2 stages]\n[c/B3FDFF:The first stage: The yoyo shoots lasers that deal 2x the yoyo's damage]\n[c/B3FDFF:The second stage: Triggers after 50 hits, the yoyo will shoot cursed flames]\n[c/B3FDFF:These flames deal half damage, but inflict cursed inferno]"));
-                        break;
-                }
+                tooltips.Add(new TooltipLine(Mod, "CounterweightStackInfo", Language.GetTextValue("Mods.CombinationsMod.LocalizedText.CounterweightPerYoyo")));
             }
             
             if ((ItemID.Sets.Yoyo[item.type] || ContentSamples.ProjectilesByType[item.shoot].aiStyle == 99))

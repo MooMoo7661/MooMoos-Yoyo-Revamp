f02a8433a812b74ab19308474f94cb18f81d67c0
Moo Moo <obsidian1392@gmail.com>
2023-09-25 10:47:47 -0600

    Got mad at shitty code and deleted it all

--- CombinationsModUtils.cs
+++ CombinationsModUtils.cs
@@ -38,217 +38,41 @@ namespace CombinationsMod
 
         public static Vector2 StringPos()
         {
-            Vector2 stringSlotPos = new Vector2();
-            int pos = ModContent.GetInstance<YoyoModConfig>().AccessorySlotPosition;
-
-            switch (pos)
-            {
-                case 1:
-                    stringSlotPos = new Vector2(Main.screenWidth - 350, Main.screenHeight / 11);
-                    break;
-
-                case 2:
-                    stringSlotPos = new Vector2(Main.screenWidth - 980, Main.screenHeight / 11);
-                    break;
-
-                case 3:
-                    stringSlotPos = new Vector2(Main.screenWidth - 1212, Main.screenHeight / 3 - 45);
-                    break;
-
-                case 4:
-                    stringSlotPos = new Vector2(Main.screenWidth / 2 + 110, (Main.screenHeight / 2 - 68));
-                    break;
-            }
-
-            return stringSlotPos;
+            return new Vector2(Main.screenWidth - 350, Main.screenHeight / 11);
         }
 
         public static Vector2 RightGlovePos()
         {
-            Vector2 rightGlovePos = new Vector2();
-            int pos = ModContent.GetInstance<YoyoModConfig>().AccessorySlotPosition;
-
-            switch (pos)
-            {
-                case 1:
-                    rightGlovePos = new Vector2(Main.screenWidth - 350, (Main.screenHeight / 11) + 50);
-                    break;
-
-                case 2:
-                    rightGlovePos = new Vector2(Main.screenWidth - 980, (Main.screenHeight / 11) + 50);
-                    break;
-
-                case 3:
-                    rightGlovePos = new Vector2(Main.screenWidth - 1212, Main.screenHeight / 3 + 2);
-                    break;
-
-                case 4:
-                    rightGlovePos = new Vector2(Main.screenWidth / 2 + 110, (Main.screenHeight / 2 - 19));
-                    break;
-            }
-
-            return rightGlovePos;
+            return new Vector2(Main.screenWidth - 350, (Main.screenHeight / 11) + 50);
         }
 
         public static Vector2 LeftGlovePos()
         {
-            Vector2 leftGlovePos = new Vector2();
-            int pos = ModContent.GetInstance<YoyoModConfig>().AccessorySlotPosition;
-
-            switch (pos)
-            {
-                case 1:
-                    leftGlovePos = new Vector2(Main.screenWidth - 397, (Main.screenHeight / 11) + 50);
-                    break;
-
-                case 2:
-                    leftGlovePos = new Vector2(Main.screenWidth - 1027, (Main.screenHeight / 11) + 50);
-                    break;
-
-                case 3:
-                    leftGlovePos = new Vector2(Main.screenWidth - 1259, (Main.screenHeight / 3 + 2));
-                    break;
-
-                case 4:
-                    leftGlovePos = new Vector2(Main.screenWidth / 2 + 63, (Main.screenHeight / 2 - 19));
-                    break;
-            }
-
-            return leftGlovePos;
+            return new Vector2(Main.screenWidth - 397, (Main.screenHeight / 11) + 50);
         }
 
         public static Vector2 CounterweightPos()
         {
-            Vector2 counterweightPos = new Vector2();
-            int pos = ModContent.GetInstance<YoyoModConfig>().AccessorySlotPosition;
-
-            switch (pos)
-            {
-                case 1:
-                    counterweightPos = new Vector2(Main.screenWidth - 350, (Main.screenHeight / 11) + 100);
-                    break;
-
-                case 2:
-                    counterweightPos = new Vector2(Main.screenWidth - 980, (Main.screenHeight / 11) + 100);
-                    break;
-
-                case 3:
-                    counterweightPos = new Vector2(Main.screenWidth - 1212, (Main.screenHeight / 3 + 49));
-                    break;
-
-                case 4:
-                    counterweightPos = new Vector2(Main.screenWidth / 2 + 110, (Main.screenHeight / 2 + 30));
-                    break;
-            }
-
-            return counterweightPos;
+            return new Vector2(Main.screenWidth - 350, (Main.screenHeight / 11) + 100);
         }
 
         public static Vector2 DrillPos()
         {
-            Vector2 drillPos = new Vector2();
-            int pos = ModContent.GetInstance<YoyoModConfig>().AccessorySlotPosition;
-
-            switch (pos)
-            {
-                case 1:
-                    drillPos = new Vector2(Main.screenWidth - 397, (Main.screenHeight / 11) + 100);
-                    break;
-
-                case 2:
-                    drillPos = new Vector2(Main.screenWidth - 1027, (Main.screenHeight / 11) + 100);
-                    break;
-
-                case 3:
-                    drillPos = new Vector2(Main.screenWidth - 1259, (Main.screenHeight / 3 + 49));
-                    break;
-
-                case 4:
-                    drillPos = new Vector2(Main.screenWidth / 2 + 63, (Main.screenHeight / 2 + 30));
-                    break;
-            }
-
-            return drillPos;
+            return new Vector2(Main.screenWidth - 397, (Main.screenHeight / 11) + 100);
         }
 
         public static Vector2 RingPos1()
         {
-            Vector2 ringPos = new Vector2();
-            int pos = ModContent.GetInstance<YoyoModConfig>().AccessorySlotPosition;
-
-            switch (pos)
-            {
-                case 1:
-                    ringPos = new Vector2(Main.screenWidth - 397, (Main.screenHeight / 11 + 150));
-                    break;
-
-                case 2:
-                    ringPos = new Vector2(Main.screenWidth - 1027, (Main.screenHeight / 11) + 150);
-                    break;
-
-                case 3:
-                    ringPos = new Vector2(Main.screenWidth - 1259, (Main.screenHeight / 3 + 96));
-                    break;
-
-                case 4:
-                    ringPos = new Vector2(Main.screenWidth / 2 + 110, (Main.screenHeight / 2 + 79));
-                    break;
-            }
-
             return new Vector2(Main.screenWidth - 397, (Main.screenHeight / 11 + 150)); 
         }
 
         public static Vector2 RingPos2()
         {
-            Vector2 ringPos = new Vector2();
-            int pos = ModContent.GetInstance<YoyoModConfig>().AccessorySlotPosition;
-
-            switch (pos)
-            {
-                case 1:
-                    ringPos = new Vector2(Main.screenWidth - 350, (Main.screenHeight / 11 + 150));
-                    break;
-
-                case 2:
-                    ringPos = new Vector2(Main.screenWidth - 980, (Main.screenHeight / 11) + 150);
-                    break;
-
-                case 3:
-                    ringPos = new Vector2(Main.screenWidth - 1212, (Main.screenHeight / 3 + 96));
-                    break;
-
-                case 4:
-                    ringPos = new Vector2(Main.screenWidth / 2 + 63, (Main.screenHeight / 2 + 79));
-                    break;
-            }
-
             return new Vector2(Main.screenWidth - 350, (Main.screenHeight / 11 + 150));
         }
 
         public static Vector2 TrickPos()
         {
-            Vector2 ringPos = new Vector2();
-            int pos = ModContent.GetInstance<YoyoModConfig>().AccessorySlotPosition;
-            //350
-            switch (pos)
-            {
-                case 1:
-                    ringPos = new Vector2(Main.screenWidth - 450, (Main.screenHeight / 11 + 77));
-                    break;
-
-                case 2:
-                    ringPos = new Vector2(Main.screenWidth - 960, (Main.screenHeight / 11) + 150);
-                    break;
-
-                case 3:
-                    ringPos = new Vector2(Main.screenWidth - 1212, (Main.screenHeight / 3 + 96));
-                    break;
-
-                case 4:
-                    ringPos = new Vector2(Main.screenWidth / 2 + 63, (Main.screenHeight / 2 + 79));
-                    break;
-            }
-
             return new Vector2(Main.screenWidth - 450, (Main.screenHeight / 11 + 77));
         }
     }

7905440de7678b814c4dce6fcdd93f3a6665610d
Moo Moo <obsidian1392@gmail.com>
2023-11-07 12:52:37 -0700

    Changed player string color -> -1. MooMooLib uses -1 as the string color to not draw

--- Items/Accessories/Strings/NaniteString.cs
+++ Items/Accessories/Strings/NaniteString.cs
@@ -12,7 +12,7 @@ namespace CombinationsMod.Items.Accessories.Strings
 {
     public class NaniteString : ModString
     {
-        public override bool CanBeUnloaded => true;
+        public override bool CanBeUnloaded => true; 
         
         public override void SetDefaults()
         {
@@ -24,22 +24,15 @@ namespace CombinationsMod.Items.Accessories.Strings
             Item.maxStack = 1;
             Item.value = Item.sellPrice(gold: 4);
             Item.hasVanityEffects = true;
-            
+            Item.stringColor = -1;
         }
 
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
             YoyoModPlayer modPlayer = player.GetModPlayer<YoyoModPlayer>();
             modPlayer.naniteString = true;
-            player.stringColor = 32;
+            player.stringColor = -1;
             player.yoyoString = true;
-        }
-
-        public override void UpdateVanity(Player player)
-        {
-            YoyoModPlayer modPlayer = player.GetModPlayer<YoyoModPlayer>();
-            
-                player.stringColor = 32; // Custom string color ID. Vanilla stops at 28, and to keep our String Info accessory working right, we create our own.
         }
     }
 }

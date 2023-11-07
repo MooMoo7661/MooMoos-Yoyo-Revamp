c9fac27c5e36d1fc62eec38ba40edf2d4758565b
Moo Moo <obsidian1392@gmail.com>
2023-11-07 12:47:50 -0700

    amogus

---
+++ ModDetector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace CombinationsMod
+{
+    public class ModDetector
+    {
+        public static bool CalamityLoaded => ModLoader.TryGetMod("CalamityMod", out Mod Calamity);
+    }
+}

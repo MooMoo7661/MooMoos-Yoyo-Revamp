225143118d32036c1eb97c71338c8ccf01deaa02
Moo Moo <obsidian1392@gmail.com>
2023-11-30 08:39:13 -0700

    Moved modsystems + added designated folder

---
+++ ModSystems/YoyoStatsSetter.cs
@@ -0,0 +1,23 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CombinationsMod.ModSystems
+{
+    public class YoyoStatsSetter : ModSystem
+    {
+        public override void PostSetupContent()
+        {
+            if (ModDetector.CalamityLoaded)
+            {
+
+            }
+        }
+    }
+
+    public class GlobalYoyoProjectile : GlobalProjectile
+    {
+        public override bool InstancePerEntity => true;
+        public override bool AppliesToEntity(Projectile projectile, bool lateInstantiation) => projectile.aiStyle == 99;
+    }
+}

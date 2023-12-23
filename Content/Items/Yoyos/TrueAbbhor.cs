631e9bb04a66bde0074a41c87c0102cecd32dc81
Moo Moo <obsidian1392@gmail.com>
2023-12-23 10:46:25 -0700

    Added glowing tooltips

--- Content/Items/Yoyos/TrueAbbhor.cs
+++ Content/Items/Yoyos/TrueAbbhor.cs
@@ -1,6 +1,7 @@
 using CombinationsMod.Content.Configs;
 using CombinationsMod.Content.Items.Bars;
 using CombinationsMod.Content.Projectiles.YoyoProjectiles;
+using CombinationsMod.Content.TrailSystem;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
@@ -9,6 +10,7 @@ using Terraria.Audio;
 using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.UI.Chat;
 
 namespace CombinationsMod.Content.Items.Yoyos
 {
@@ -41,17 +43,85 @@ namespace CombinationsMod.Content.Items.Yoyos
             Item.value = Item.sellPrice(0, 15, 90, 83);
             Item.shoot = ModContent.ProjectileType<TrueAbbhorProjectile>();
 
-
+            manager = new CompactParticleManager(
+            particle =>
+            {
+                particle.Rotation = 0;
+                if (particle.TimeAlive == 0)
+                {
+                    particle.Scale = 0f;
+                    particle.Opacity = 1f;
+                }
+                else if (particle.TimeAlive < 30)
+                {
+                    particle.Scale = MathHelper.Lerp(particle.Scale, 1f, 0.1f);
+                    particle.Opacity = MathHelper.Lerp(particle.Opacity, 0f, 0.1f);
+                }
+                else if (particle.TimeAlive < 60)
+                {
+                    particle.Scale = MathHelper.Lerp(particle.Scale, 0f, 0.1f);
+                    particle.Opacity = MathHelper.Lerp(particle.Opacity, 1f, 0.1f);
+                }
+                else
+                {
+                    particle.Dead = true;
+                }
+            },
+            (particle, spriteBatch, anchor) =>
+            {
+                Texture2D texture = ModContent
+                    .Request<Texture2D>("Terraria/Images/Projectile_" + ProjectileID.RainbowCrystalExplosion).Value;
+                spriteBatch.Draw(texture, anchor + particle.Position, null, particle.Color * particle.Opacity, 0f, texture.Size() / 2, particle.Scale * 0.5f, SpriteEffects.None, 0f);
+                spriteBatch.Draw(texture, anchor + particle.Position, null, particle.Color * particle.Opacity, MathHelper.PiOver2, texture.Size() / 2, particle.Scale * 0.5f, SpriteEffects.None, 0f);
+                spriteBatch.Draw(texture, anchor + particle.Position, null, particle.Color * particle.Opacity * 0.5f, 0f, texture.Size() / 2, particle.Scale * 0.75f, SpriteEffects.None, 0f);
+                spriteBatch.Draw(texture, anchor + particle.Position, null, particle.Color * particle.Opacity * 0.5f, MathHelper.PiOver2, texture.Size() / 2, particle.Scale * 0.75f, SpriteEffects.None, 0f);
+            }
+        );
         }
+
+        CompactParticleManager manager;
+        public override bool PreDrawTooltipLine(DrawableTooltipLine line, ref int yOffset)
+        {
+            if (line.Name == "ItemName" && line.Mod == "Terraria")
+            {
+                Color color = Color.Lerp(new Color(226, 35, 89), new Color(230, 96, 132), (MathF.Sin(Main.GlobalTimeWrappedHourly * 0.7f) + 1) / 2f);
+
+                TrailSystem.Utils.Reload(Main.spriteBatch, BlendState.Additive);
+                Vector2 bounds = FontAssets.MouseText.Value.MeasureString(line.Text);
+                TrailSystem.Utils.Reload(Main.spriteBatch, BlendState.AlphaBlend);
+
+
+                if (Main.GameUpdateCount % 13 == 0)
+                {
+                    bounds.Y -= 10;
+                    Vector2 pos = new Vector2(0, 5) + new Vector2(Main.rand.NextFloat(bounds.X), Main.rand.NextFloat(bounds.Y));
+                    manager.AddParticle(pos, (pos - bounds / 2).SafeNormalize(Vector2.UnitY) * 1, 0f, 5f, 1f, new Color(226, 35, 89));
+                }
+                manager.Update();
+
+                ChatManager.DrawColorCodedStringShadow(Main.spriteBatch, FontAssets.MouseText.Value, line.Text,
+                    new Vector2(line.X, line.Y), Color.Black, 0f, Vector2.Zero, line.BaseScale);
+
+                ChatManager.DrawColorCodedString(Main.spriteBatch, FontAssets.MouseText.Value, line.Text,
+                    new Vector2(line.X, line.Y), color, 0f, Vector2.Zero, Vector2.One);
+
+                TrailSystem.Utils.Reload(Main.spriteBatch, BlendState.Additive);
+                manager.Draw(Main.spriteBatch, new Vector2(line.X, line.Y));
+                TrailSystem.Utils.Reload(Main.spriteBatch, BlendState.AlphaBlend);
+                return false;
+            }
+            return base.PreDrawTooltipLine(line, ref yOffset);
+        }
+
         public override void AddRecipes()
         {
-            Recipe recipe = CreateRecipe();
-            recipe.AddIngredient(ModContent.ItemType<TheAbbhor>());
-            recipe.AddIngredient(ModContent.ItemType<MightBar>(), 10);
-            recipe.AddIngredient(ModContent.ItemType<SightBar>(), 10);
-            recipe.AddIngredient(ModContent.ItemType<FrightBar>(), 10);
-            recipe.AddTile(TileID.MythrilAnvil);
-            recipe.Register();
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<TheAbbhor>())
+                .AddIngredient(ModContent.ItemType<MightBar>(), 10)
+                .AddIngredient(ModContent.ItemType<SightBar>(), 10)
+                .AddIngredient(ModContent.ItemType<FrightBar>(), 10)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
         }
 
         public override bool IsLoadingEnabled(Mod mod)

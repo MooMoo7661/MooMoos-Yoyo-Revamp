b7e05bba2f0793aa0450a6b911d0d440c76a89e3
Moo Moo <obsidian1392@gmail.com>
2023-12-27 14:46:12 -0700

    Added more cool tooltip drawing

--- Content/Items/Accessories/Rings/TerreneRing.cs
+++ Content/Items/Accessories/Rings/TerreneRing.cs
@@ -1,9 +1,15 @@
 using CombinationsMod.Content.Items.Bars;
 using CombinationsMod.Content.Items.Misc;
 using CombinationsMod.Content.ModPlayers;
+using CombinationsMod.Content.TrailSystem;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using Terraria;
+using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.UI.Chat;
 
 namespace CombinationsMod.Content.Items.Accessories.Rings
 {
@@ -19,6 +25,76 @@ namespace CombinationsMod.Content.Items.Accessories.Rings
             Item.accessory = true;
             Item.maxStack = 1;
             Item.value = Item.sellPrice(0, 30, 5, 0);
+
+            manager = new CompactParticleManager(
+            particle =>
+            {
+                particle.Rotation = 0;
+                if (particle.TimeAlive == 0)
+                {
+                    particle.Scale = 0f;
+                    particle.Opacity = 1f;
+                }
+                else if (particle.TimeAlive < 30)
+                {
+                    particle.Scale = MathHelper.Lerp(particle.Scale, 1f, 0.1f);
+                    particle.Opacity = MathHelper.Lerp(particle.Opacity, 0f, 0.1f);
+                }
+                else if (particle.TimeAlive < 60)
+                {
+                    particle.Scale = MathHelper.Lerp(particle.Scale, 0f, 0.1f);
+                    particle.Opacity = MathHelper.Lerp(particle.Opacity, 1f, 0.1f);
+                }
+                else
+                {
+                    particle.Dead = true;
+                }
+            },
+            (particle, spriteBatch, anchor) =>
+            {
+                Texture2D texture = ModContent
+                    .Request<Texture2D>("Terraria/Images/Projectile_" + ProjectileID.RainbowCrystalExplosion).Value;
+                spriteBatch.Draw(texture, anchor + particle.Position, null, particle.Color * particle.Opacity, 0f, texture.Size() / 2, particle.Scale * 0.5f, SpriteEffects.None, 0f);
+                spriteBatch.Draw(texture, anchor + particle.Position, null, particle.Color * particle.Opacity, MathHelper.PiOver2, texture.Size() / 2, particle.Scale * 0.5f, SpriteEffects.None, 0f);
+                spriteBatch.Draw(texture, anchor + particle.Position, null, particle.Color * particle.Opacity * 0.5f, 0f, texture.Size() / 2, particle.Scale * 0.75f, SpriteEffects.None, 0f);
+                spriteBatch.Draw(texture, anchor + particle.Position, null, particle.Color * particle.Opacity * 0.5f, MathHelper.PiOver2, texture.Size() / 2, particle.Scale * 0.75f, SpriteEffects.None, 0f);
+            }
+        );
+        }
+
+        CompactParticleManager manager;
+        public override bool PreDrawTooltipLine(DrawableTooltipLine line, ref int yOffset)
+        {
+            if (line.Name == "ItemName" && line.Mod == "Terraria")
+            {
+                Color color = Color.Lerp(Color.HotPink, Color.DarkBlue, (MathF.Sin(Main.GlobalTimeWrappedHourly * 2.9f) + 1) / 2f);
+
+                TrailSystem.Utils.Reload(Main.spriteBatch, BlendState.Additive);
+                Vector2 bounds = FontAssets.MouseText.Value.MeasureString(line.Text);
+                TrailSystem.Utils.Reload(Main.spriteBatch, BlendState.AlphaBlend);
+
+
+                if (Main.GameUpdateCount % 9 == 0)
+                {
+                    bounds.Y -= 10;
+                    Vector2 pos = new Vector2(0, 5) + new Vector2(Main.rand.NextFloat(bounds.X), Main.rand.NextFloat(bounds.Y));
+                    Color color2 = Color.MediumPurple;
+                    manager.AddParticle(pos, (pos - bounds / 2).SafeNormalize(Vector2.UnitY) * 1, 0f, 5f, 1f, color2);
+                }
+                manager.Update();
+
+                ChatManager.DrawColorCodedStringShadow(Main.spriteBatch, FontAssets.MouseText.Value, line.Text,
+                    new Vector2(line.X, line.Y), Color.WhiteSmoke, 0f, Vector2.Zero, line.BaseScale);
+
+                ChatManager.DrawColorCodedString(Main.spriteBatch, FontAssets.MouseText.Value, line.Text,
+                    new Vector2(line.X, line.Y), color, 0f, Vector2.Zero, Vector2.One);
+
+                TrailSystem.Utils.Reload(Main.spriteBatch, BlendState.Additive);
+                manager.Draw(Main.spriteBatch, new Vector2(line.X, line.Y));
+                TrailSystem.Utils.Reload(Main.spriteBatch, BlendState.AlphaBlend);
+                return false;
+            }
+            return base.PreDrawTooltipLine(line, ref yOffset);
         }
 
         public override void UpdateAccessory(Player player, bool hideVisual)

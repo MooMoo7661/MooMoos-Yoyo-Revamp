fdb61dba2bc6ac3865ef422aba41f0908efbecbe
Moo Moo <obsidian1392@gmail.com>
2024-01-22 09:29:25 -0700

    Removed old dusts + added 3 brick dusts

---
+++ Content/Dusts/MightBarTileDust.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CombinationsMod.Content.Dusts
+{
+    public class MightBarTileDust : ModDust
+    {
+        public override void SetStaticDefaults()
+        {
+            Dust.CloneDust(DustID.CrystalPulse2);
+        }
+    }
+
+    public class SightBarTileDust : ModDust
+    {
+        public override void SetStaticDefaults()
+        {
+            Dust.CloneDust(DustID.CrystalPulse2);
+        }
+    }
+
+    public class FrightBarTileDust : ModDust
+    {
+        public override void SetStaticDefaults()
+        {
+            Dust.CloneDust(DustID.CrystalPulse2);
+        }
+    }
+}

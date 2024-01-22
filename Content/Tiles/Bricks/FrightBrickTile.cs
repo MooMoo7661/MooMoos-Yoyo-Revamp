e30e2f30a95c0b26e86f7963a09b4886d1658c98
Moo Moo <obsidian1392@gmail.com>
2024-01-22 09:22:09 -0700

    Added brick tiles for might bars

---
+++ Content/Tiles/Bricks/FrightBrickTile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using CombinationsMod.Content.Dusts;
+using CombinationsMod.Content.Items.Bars;
+using Microsoft.Xna.Framework;
+using CombinationsMod.Content.Items.Tiles;
+
+namespace CombinationsMod.Content.Tiles.Bricks
+{
+    public class FrightBrickTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileMerge[Type][Type] = true;
+            Main.tileSolid[Type] = true;
+            Main.tileLighted[Type] = false;
+            Main.tileNoSunLight[Type] = false;
+            Main.tileBlockLight[Type] = true;
+            DustType = ModContent.DustType<FrightBarTileDust>();
+            HitSound = SoundID.Tink;
+            RegisterItemDrop(ModContent.ItemType<FrightBrick>());
+            AddMapEntry(Color.IndianRed);
+        }
+
+        public override void KillTile(int i, int j, ref bool fail, ref bool effectOnly, ref bool noItem)
+        {
+            Lighting.AddLight(new Vector2(i, j) * 16, TorchID.Red);
+            for (int x = 0; x < 4; x++)
+                Dust.NewDust(new Vector2(i, j) * 16, 16, 16, DustID.RedTorch);
+        }
+    }
+}
---
+++ Content/Tiles/Bricks/SightBrickTile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using CombinationsMod.Content.Dusts;
+using CombinationsMod.Content.Items.Bars;
+using Microsoft.Xna.Framework;
+using CombinationsMod.Content.Items.Tiles;
+
+namespace CombinationsMod.Content.Tiles.Bricks
+{
+    public class SightBrickTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileMerge[Type][Type] = true;
+            Main.tileSolid[Type] = true;
+            Main.tileLighted[Type] = false;
+            Main.tileNoSunLight[Type] = false;
+            Main.tileBlockLight[Type] = true;
+            DustType = ModContent.DustType<SightBarTileDust>();
+            HitSound = SoundID.Tink;
+            RegisterItemDrop(ModContent.ItemType<SightBrick>());
+            AddMapEntry(Color.LawnGreen);
+        }
+
+        public override void KillTile(int i, int j, ref bool fail, ref bool effectOnly, ref bool noItem)
+        {
+            Lighting.AddLight(new Vector2(i, j) * 16, TorchID.Green);
+            for (int x = 0; x < 4; x++)
+                Dust.NewDust(new Vector2(i, j) * 16, 16, 16, DustID.GreenTorch);
+        }
+    }
+}

1aaf027bf89e50d957bad691d8d661da8196e695
Moo Moo <obsidian1392@gmail.com>
2024-08-09 01:28:41 -0600

    Removed EOC yoyo progression change and all associated code

--- Content/Global Classes/Loot/MothronLootRemover.cs
+++ Content/Global Classes/Loot/MothronLootRemover.cs
@@ -5,24 +5,24 @@ using Terraria.ModLoader;
 
 namespace CombinationsMod.GlobalClasses.Loot
 {
-    public class MothronLootRemover : GlobalNPC
-    {
-        public override bool AppliesToEntity(NPC entity, bool lateInstantiation) => entity.type == NPCID.Mothron;
-        public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
-        {
-            foreach (var rule in npcLoot.Get())
-            {
-                if (rule is LeadingConditionRule downedAllMechBossesRule && downedAllMechBossesRule.condition is Conditions.DownedAllMechBosses)
-                {
-                    foreach (var chain in downedAllMechBossesRule.ChainedRules)
-                    {
-                        if (chain.RuleToChain is LeadingConditionRule downedPlanteraRule && downedPlanteraRule.condition is Conditions.DownedPlantera)
-                        {
-                            downedPlanteraRule.ChainedRules.RemoveAll(chain => chain.RuleToChain is DropBasedOnExpertMode expertDrop && expertDrop.ruleForExpertMode is CommonDropWithRerolls rerollDrop && rerollDrop.itemId == ItemID.TheEyeOfCthulhu);
-                        }
-                    }
-                }
-            }
-        }
-    }
+    //public class MothronLootRemover : GlobalNPC
+    //{
+    //    public override bool AppliesToEntity(NPC entity, bool lateInstantiation) => entity.type == NPCID.Mothron;
+    //    public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
+    //    {
+    //        foreach (var rule in npcLoot.Get())
+    //        {
+    //            if (rule is LeadingConditionRule downedAllMechBossesRule && downedAllMechBossesRule.condition is Conditions.DownedAllMechBosses)
+    //            {
+    //                foreach (var chain in downedAllMechBossesRule.ChainedRules)
+    //                {
+    //                    if (chain.RuleToChain is LeadingConditionRule downedPlanteraRule && downedPlanteraRule.condition is Conditions.DownedPlantera)
+    //                    {
+    //                        downedPlanteraRule.ChainedRules.RemoveAll(chain => chain.RuleToChain is DropBasedOnExpertMode expertDrop && expertDrop.ruleForExpertMode is CommonDropWithRerolls rerollDrop && rerollDrop.itemId == ItemID.TheEyeOfCthulhu);
+    //                    }
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
 }

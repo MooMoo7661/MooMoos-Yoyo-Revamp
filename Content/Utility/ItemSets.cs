3c33197b5b37f0c6db02fe68a862b74b2ffe3dab
Moo Moo <obsidian1392@gmail.com>
2024-08-09 01:47:26 -0600

    Item sets are now applied in the item's setdefaults

--- Content/Utility/ItemSets.cs
+++ Content/Utility/ItemSets.cs
@@ -41,81 +41,7 @@ namespace CombinationsMod.Content.Utility
                 YoyoString[veridianMod.Find<ModItem>("HellString").Type] = true;
             }
 
-            #region YoyoString
-            YoyoString[ItemType<DarkBlueString>()] = true;
-            YoyoString[ItemType<DarkGreenString>()] = true;
-            YoyoString[ItemType<DarkTealString>()] = true;
-            YoyoString[ItemType<EclipseString>()] = true;
-            YoyoString[ItemType<FrostbiteString>()] = true;
-            YoyoString[ItemType<GolemsteelString>()] = true;
-            YoyoString[ItemType<GrapeString>()] = true;
-            YoyoString[ItemType<LightPinkString>()] = true;
-            YoyoString[ItemType<NaniteString>()] = true;
-            YoyoString[ItemType<NebulaString>()] = true;
-            YoyoString[ItemType<SlimyString>()] = true;
-            YoyoString[ItemType<SolarString>()] = true;
-            YoyoString[ItemType<StardustString>()] = true;
-            YoyoString[ItemType<VortexString>()] = true;
-            #endregion
-
-            #region YoyoBag
-            YoyoBag[ItemType<AdvancedYoyoBag>()] = true;
-            YoyoBag[ItemType<BeetleBag>()] = true;
-            YoyoBag[ItemType<MoonlordBag>()] = true;
-            YoyoBag[ItemType<ShimmerBag>()] = true;
-            YoyoBag[ItemType<ShimmeringBeetleBag>()] = true;
-            YoyoBag[ItemType<Tier2Bag>()] = true;
-            #endregion
-
             SupportGlove[ItemType<SupportGlove>()] = true;
-
-            #region Drills
-            DrillCasing[ItemType<AdamantiteDrillCasing>()] = true;
-            DrillCasing[ItemType<CelestialDrillCasing>()] = true;
-            DrillCasing[ItemType<CobaltDrillCasing>()] = true;
-            DrillCasing[ItemType<ExcavatorCasing>()] = true;
-            DrillCasing[ItemType<HakapikDrillCasing>()] = true;
-            DrillCasing[ItemType<HorsemansDrillCasing>()] = true;
-            DrillCasing[ItemType<YoyoDrillCasing>()] = true;
-            DrillCasing[ItemType<MattockDrillCasing>()] = true;
-            DrillCasing[ItemType<MooMooDrillCasing>()] = true;
-            DrillCasing[ItemType<MythrilDrillCasing>()] = true;
-            DrillCasing[ItemType<NebulaDrillCasing>()] = true;
-            DrillCasing[ItemType<OrichalcumDrillCasing>()] = true;
-            DrillCasing[ItemType<PalladiumCasing>()] = true;
-            DrillCasing[ItemType<ShroomiteShredderCasing>()] = true;
-            DrillCasing[ItemType<SolarDrillCasing>()] = true;
-            DrillCasing[ItemType<SpectralShredderCasing>()] = true;
-            DrillCasing[ItemType<StardustDrillCasing>()] = true;
-            DrillCasing[ItemType<TitaniumDrillCasing>()] = true;
-            DrillCasing[ItemType<TreeClippersDrillCasing>()] = true;
-            DrillCasing[ItemType<TsurugiDrillCasing>()] = true;
-            DrillCasing[ItemType<VortexDrillCasing>()] = true;
-            #endregion
-
-            #region Rings
-            YoyoRing[ItemType<AbilityRing>()] = true;
-            YoyoRing[ItemType<AmberRing>()] = true;
-            YoyoRing[ItemType<AmethystRing>()] = true;
-            YoyoRing[ItemType<DiamondRing>()] = true;
-            YoyoRing[ItemType<EmeraldRing>()] = true;
-            YoyoRing[ItemType<FortitudeRing>()] = true;
-            YoyoRing[ItemType<GemstoneRing>()] = true;
-            YoyoRing[ItemType<OmnipotenceRing>()] = true;
-            YoyoRing[ItemType<RingOfCoalescence>()] = true;
-            YoyoRing[ItemType<RubyRing>()] = true;
-            YoyoRing[ItemType<SapphireRing>()] = true;
-            YoyoRing[ItemType<TerreneRing>()] = true;
-            YoyoRing[ItemType<TopazRing>()] = true;
-            YoyoRing[ItemType<TrepidationRing>()] = true;
-            #endregion
-
-            #region Tricks
-            Trick[ItemType<AroundTheWorld>()] = true;
-            Trick[ItemType<AroundTheWorldTier2>()] = true;
-            Trick[ItemType<ShootToTheMoon>()] = true;
-            Trick[ItemType<DualYoyo>()] = true;
-            #endregion
         }
 
         public static bool[] YoyoString = ItemID.Sets.Factory.CreateBoolSet(
@@ -133,7 +59,8 @@ namespace CombinationsMod.Content.Utility
             ItemID.VioletString,
             ItemID.WhiteString,
             ItemID.YellowString,
-            ItemID.RedString
+            ItemID.RedString,
+            ItemID.RainbowString
             );
 
         public static bool[] YoyoBag = ItemID.Sets.Factory.CreateBoolSet(

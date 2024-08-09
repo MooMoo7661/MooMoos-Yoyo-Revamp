ba1acad7cbd953c54526dd62e27428818d062967
Moo Moo <obsidian1392@gmail.com>
2024-08-09 01:42:59 -0600

    Added misc modplayer for other modplayer methods, to reserve the main one for the detouring logic and fields

---
+++ Content/ModPlayers/MiscYoyoModPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.Localization;
+using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace CombinationsMod.Content.ModPlayers
+{
+    public class MiscYoyoModPlayer : ModPlayer
+    {
+        public override void PlayerDisconnect() // Subtracts the added Yoyo stats when the player disconnects.
+        {
+            Player.GetModPlayer<YoyoModPlayer>().HitCounter = 0;
+        }
+
+        public override void OnEnterWorld()
+        {
+            if (!ModLoader.TryGetMod("TMLAchievements", out Mod mod))
+                Main.NewText(Language.GetTextValue("Mods.CombinationsMod.LocalizedText.AchievementsNotification"), Color.LightGoldenrodYellow);
+        }
+
+        public override void PostUpdateEquips()
+        {
+            YoyoModPlayer modPlayer = Player.GetModPlayer<YoyoModPlayer>();
+
+            if (modPlayer.beetleBag || modPlayer.moonlordBag || modPlayer.shimmerBag || modPlayer.tier2Bag || modPlayer.yoyoBag)
+            {
+                modPlayer.playerHasYoyoBagEquipped = true;
+            }
+        }
+    }
+}

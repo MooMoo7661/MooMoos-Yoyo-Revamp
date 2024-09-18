b08e9eb7a52c29b19b326d4a7b4309f0c4991983
Moo Moo <obsidian1392@gmail.com>
2024-09-18 11:37:47 -0600

    New data system for yoyos, stores yoyo-specific data. Mostly for detecting the main yoyo.

---
+++ Content/Global Classes/Projectiles/YoyoDataHouse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+
+namespace CombinationsMod.Content.Global_Classes.Projectiles
+{
+    // Naming (mostly) unrelated to a certain vicodin-fueled individual
+    public class YoyoDataHouse : GlobalProjectile
+    {
+        public override bool InstancePerEntity => true;
+        public override bool AppliesToEntity(Projectile proj, bool lateInstantiation) => proj.aiStyle == 99;
+
+        public int hits = 0;
+        public bool mainYoyo = false;
+
+        public override void OnSpawn(Projectile proj, IEntitySource source)
+        {
+            mainYoyo = proj.ai[2] == 0;
+        }
+    }
+}

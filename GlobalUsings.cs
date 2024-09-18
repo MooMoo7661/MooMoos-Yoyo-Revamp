76896a244a5f758ac2c9b7599d9485ec7f131c2c
Moo Moo <obsidian1392@gmail.com>
2024-09-18 12:01:28 -0600

    I'm lazy

---
+++ GlobalUsings.cs
@@ -0,0 +1,11 @@
+﻿global using Microsoft.Xna.Framework;
+global using CombinationsMod.Content.Utility;
+global using Terraria;
+global using Terraria.ModLoader;
+global using Terraria.Utilities;
+global using Terraria.ID;
+global using Terraria.GameContent;
+global using Terraria.Localization;
+
+global using Vector2 = Microsoft.Xna.Framework.Vector2;
+global using Color = Microsoft.Xna.Framework.Color;

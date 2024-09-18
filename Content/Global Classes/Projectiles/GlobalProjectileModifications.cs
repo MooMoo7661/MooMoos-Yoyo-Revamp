23401342e9591ec0115d74fdafd89b29d551e218
Moo Moo <obsidian1392@gmail.com>
2024-09-18 11:35:59 -0600

    Re-organization, code fixes, and added changes to main yoyo detection system

--- Content/Global Classes/Projectiles/GlobalProjectileModifications.cs
+++ Content/Global Classes/Projectiles/GlobalProjectileModifications.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Reflection;
+using CombinationsMod.Content.Global_Classes.Projectiles;
 using CombinationsMod.Content.ModPlayers;
 using CombinationsMod.Content.Projectiles.Explosions;
 using CombinationsMod.Content.Projectiles.TrickYoyos;
 using CombinationsMod.Content.Projectiles.YoyoEffects.Solid;
+using CombinationsMod.Content.Utility;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.DataStructures;
@@ -16,44 +18,41 @@ namespace CombinationsMod.GlobalClasses.Projectiles
     public class GlobalProjectileModifications : GlobalProjectile
     {
         public override bool InstancePerEntity => true;
-        private readonly bool isOriginalYoyo;
-        private int cursedFlamesCounter = 0;
+
         private int slimeThornCounter = 0;
         private int lifestealCooldown = 0;
-
-        // This class mostly does things with yoyos. The reason it's not part of VanillaYoyoEffects class is because this applies to all
-        // yoyos, and not just vanilla ones. I felt that the things in here fit better where they are.
 
         public override void OnSpawn(Projectile projectile, IEntitySource source)
         {
             lifestealCooldown += Main.rand.Next(10);
-            Player player = Main.player[projectile.owner];
+
+            Player player = projectile.GetOwner();
             YoyoModPlayer modPlayer = player.GetModPlayer<YoyoModPlayer>();
 
-            if (projectile.aiStyle == 99 && !projectile.counterweight) // Checking if yoyo but not counterweight
+            if (projectile.IsYoyo() && projectile.owner == Main.myPlayer)
             {
-                if (modPlayer.trick1 && projectile.owner == Main.myPlayer)
-                {
-                    Projectile.NewProjectile(projectile.GetSource_FromThis(), player.Center,
-                    new(0, 0), ModContent.ProjectileType<World1>(), (int)(projectile.damage * 0.3f), 0, Main.myPlayer, 0, projectile.whoAmI);
-                }
-                else if (modPlayer.trick2 && projectile.owner == Main.myPlayer)
+                if (modPlayer.trick2)
                 {
                     for (int i = 0; i < 2; i++)
                     {
                         Projectile.NewProjectile(projectile.GetSource_FromThis(), player.Center,
-                        new(0, 0), ModContent.ProjectileType<World2>(), (int)(projectile.damage * 0.5f), 0, Main.myPlayer, 0, projectile.whoAmI);
+                        Vector2.Zero, ModContent.ProjectileType<World2>(), (int)(projectile.damage * 0.5f), 0, Main.myPlayer, 0, projectile.whoAmI);
                     }
+                }
+                else if (modPlayer.trick1)
+                {
+                    Projectile.NewProjectile(projectile.GetSource_FromThis(), player.Center,
+                    Vector2.Zero, ModContent.ProjectileType<World1>(), (int)(projectile.damage * 0.3f), 0, Main.myPlayer, 0, projectile.whoAmI);
                 }
             }
         }
 
         public override void ModifyHitNPC(Projectile projectile, NPC target, ref NPC.HitModifiers modifiers)
         {
-            Player player = Main.player[projectile.owner];
+            Player player = projectile.GetOwner();
             YoyoModPlayer modPlayer = player.GetModPlayer<YoyoModPlayer>();
 
-            if (modPlayer.golemString && projectile.aiStyle == 99 && projectile.counterweight)
+            if (modPlayer.golemString && projectile.IsCounterweight())
             {
                 modifiers.FinalDamage *= 2f;
                 modifiers.Knockback *= 2f;
@@ -62,101 +61,76 @@ namespace CombinationsMod.GlobalClasses.Projectiles
 
         public override void OnHitNPC(Projectile projectile, NPC target, NPC.HitInfo hit, int damageDone)
         {
-            Player player = Main.player[projectile.owner];
+            Player player = projectile.GetOwner();
             YoyoModPlayer modPlayer = player.GetModPlayer<YoyoModPlayer>();
 
-            if (projectile.aiStyle == 99 && modPlayer.hitTracker && !projectile.counterweight && ModContent.GetInstance<VanillaYoyoEffects>().ReturnProjectileFlag(projectile))
-                modPlayer.HitCounter++;
-
-            if (projectile.aiStyle == 99 && !projectile.counterweight)
+            if (projectile.IsYoyo())
             {
-                cursedFlamesCounter++;
-
-                if (modPlayer.eclipseString && Main.myPlayer == projectile.owner)
+                projectile.GetGlobalProjectile<YoyoDataHouse>().hits++;
+   
+                if (projectile.YoyoData().mainYoyo)
                 {
-                    if (Main.rand.NextBool(4))
-                    {
-                        Vector2 velocity = Vector2.One.RotatedByRandom(MathHelper.TwoPi) * 1f;
+                    modPlayer.HitCounter++;
 
-                        if (Main.rand.NextBool())
-                        {
-                            Projectile.NewProjectile(projectile.GetSource_FromThis(), projectile.Center, velocity,
-                                    ModContent.ProjectileType<EclipseSwirl>(), 0, 0, projectile.owner, 0, 1f);
-                        }
-                        else
-                        {
-                            Projectile.NewProjectile(projectile.GetSource_FromThis(), projectile.Center, velocity,
-                                    ModContent.ProjectileType<EclipseSwirlOrange>(), 0, 0, projectile.owner, 0, 1f);
-                        }
+                    if (modPlayer.eclipseString && Main.rand.NextBool(4))
+                    {
+                        Vector2 velocity = Vector2.One.RotatedByRandom(MathHelper.TwoPi);
+
+                        Projectile.NewProjectile(projectile.GetSource_FromThis(), projectile.Center, velocity,
+                               Main.rand.NextBool() ? ModContent.ProjectileType<EclipseSwirl>() : ModContent.ProjectileType<EclipseSwirlOrange>(), 0, 0, projectile.owner, 0, 1f);
 
                         Projectile projHitbox = Projectile.NewProjectileDirect(projectile.GetSource_FromThis(), projectile.Center, velocity,
                                 ModContent.ProjectileType<SolidHitbox>(), 75, 4f, projectile.owner, 0, 1f);
+
                         projHitbox.Resize(100, 100);
                     }
                 }
 
-                if (modPlayer.omnipotenceRing && isOriginalYoyo && cursedFlamesCounter % 3 == 0 && Main.myPlayer == projectile.owner)
+                if (!target.immortal && !target.dontTakeDamage && target.lifeMax > 10 && lifestealCooldown >= 15 + (projectile.MaxUpdates * 10) + (projectile.usesLocalNPCImmunity ? projectile.localNPCHitCooldown * 0.7f : 0) && modPlayer.lifestealTrick)
                 {
-                    Vector2 velocity = Vector2.One.RotatedByRandom(MathHelper.TwoPi) * 3f;
+                    Projectile.NewProjectile(projectile.GetSource_FromThis(), projectile.Center, Vector2.Zero,
+                        ProjectileID.VampireHeal, 0, 0, projectile.owner, projectile.owner, Math.Clamp(projectile.damage / 20f, 1f, 3f) + Main.rand.Next(3));
 
-                    Projectile proj = Projectile.NewProjectileDirect(projectile.GetSource_FromThis(), projectile.Center, velocity,
-                        ProjectileID.CursedFlameFriendly, 54, 4f, projectile.owner, 0, 1f);
-                    proj.scale = 1f;
-                    proj.tileCollide = true;
-                    proj.timeLeft = 120;
-                    proj.friendly = true;
-                    proj.hostile = false;
-                    proj.netUpdate = true;
+                    lifestealCooldown = 0;
                 }
             }
-            else if (projectile.counterweight && projectile.aiStyle == 99)
+            else if (projectile.IsCounterweight())
             {
-                if (modPlayer.golemString)
+                if (modPlayer.golemString && Main.myPlayer == projectile.owner)
                 {
-                    if (Main.myPlayer == projectile.owner)
-                    {
-                        Projectile proj = Projectile.NewProjectileDirect(projectile.GetSource_FromThis(), new(projectile.Center.X, projectile.Center.Y - 1f), new(Main.rand.NextBool() ? 1 : -1,
-                        Main.rand.NextBool() ? 1 : -1), ModContent.ProjectileType<EclipseExplosion>(), 11, 0, projectile.owner);
-
-                        proj.damage = 84;
-                    }
+                    Projectile.NewProjectileDirect(projectile.GetSource_FromThis(), projectile.Center, Vector2.Zero, ModContent.ProjectileType<EclipseExplosion>(), 84, 0, projectile.owner);
                 }
 
                 if (modPlayer.frostbiteString)
                     target.AddBuff(BuffID.Frostburn2, 300);
             }
+
             if (projectile.type == ProjectileID.Stinger)
             {
-                int rand = Main.rand.Next(2);
-                if (rand == 0)
+                if (Main.rand.NextBool(2))
                     target.AddBuff(BuffID.Poisoned, 240);
-            }
-                
-            if (lifestealCooldown < 15 + (projectile.MaxUpdates * 10) + (projectile.usesLocalNPCImmunity ? projectile.localNPCHitCooldown * 0.7f : 0    ) && modPlayer.lifestealTrick)
-            {
-                Projectile.NewProjectile(projectile.GetSource_FromThis(), projectile.Center, Vector2.Zero,
-                ProjectileID.VampireHeal, 0, 0, projectile.owner, projectile.owner, Math.Clamp(projectile.damage / 20f, 1f, 3f) + Main.rand.Next(3));
-                lifestealCooldown = 0;
             }
         }
         public override void AI(Projectile projectile)
         {
-            Player player = Main.player[projectile.owner];
-            YoyoModPlayer modPlayer = player.GetModPlayer<YoyoModPlayer>(); //  Getting an instance of the player and the ModPlayer
+            Player player = projectile.GetOwner();
+            YoyoModPlayer modPlayer = player.GetModPlayer<YoyoModPlayer>();
 
-            if (projectile.aiStyle == 99 && !projectile.counterweight)
+            if (projectile.IsYoyo())
             {
-                slimeThornCounter++;
+                if (lifestealCooldown < 15 + (projectile.MaxUpdates * 10) + (projectile.usesLocalNPCImmunity ? projectile.localNPCHitCooldown * 0.7f : 0))
+                    lifestealCooldown++;
 
                 if (modPlayer.slimeString)
                 {
+                    slimeThornCounter++;
                     if (slimeThornCounter == 60)
                     {
                         Vector2 velocity = Vector2.One.RotatedByRandom(MathHelper.TwoPi) * 3f;
                         if (Main.myPlayer == projectile.owner)
                         {
                             Projectile projSlimeSpike = Projectile.NewProjectileDirect(projectile.GetSource_FromThis(), projectile.Center, velocity,
-                                        ProjectileID.SpikedSlimeSpike, projectile.damage / 2, 3f, projectile.owner, 0, 1f);
+                            ProjectileID.SpikedSlimeSpike, projectile.damage / 2, 3f, projectile.owner, 0, 1f);
                             projSlimeSpike.friendly = true;
                             projSlimeSpike.hostile = false;
                         }
@@ -164,17 +138,15 @@ namespace CombinationsMod.GlobalClasses.Projectiles
                         slimeThornCounter = 0;
                     }
                 }
-
-                if (lifestealCooldown < 15 + (projectile.MaxUpdates * 10) + (projectile.usesLocalNPCImmunity ? projectile.localNPCHitCooldown * 0.7f : 0))
-                    lifestealCooldown++;
             }
         }
 
-        public override void PostAI(Projectile projectile) // Using this mostly to make vanilla yoyos have certain dust effects.
+        public override void PostAI(Projectile projectile)
         {
-            Player player = Main.player[projectile.owner];
+            Player player = projectile.GetOwner();
             YoyoModPlayer modPlayer = player.GetModPlayer<YoyoModPlayer>();
-            if (projectile.aiStyle == 99 && !projectile.counterweight)
+
+            if (projectile.IsYoyo())
             {
                 if (modPlayer.amberRing)
                 {
@@ -290,25 +262,27 @@ namespace CombinationsMod.GlobalClasses.Projectiles
             }
         }
 
-        public override void OnKill(Projectile projectile, int timeLeft) // Resetting counters when the yoyo is killed.
+        // Resetting counters when the yoyo is killed.
+        public override void OnKill(Projectile projectile, int timeLeft)
         {
-            if (projectile.aiStyle == 99 && !projectile.counterweight && ModContent.GetInstance<VanillaYoyoEffects>().ReturnProjectileFlag(projectile))
-            {
-                Player player = Main.player[projectile.owner];
-                YoyoModPlayer modPlayer = player.GetModPlayer<YoyoModPlayer>();
-                modPlayer.HitCounter = 0;
-            }
+            if (projectile.IsYoyo() && projectile.YoyoData().mainYoyo)
+                projectile.GetOwner().GetModPlayer<YoyoModPlayer>().HitCounter = 0;
         }
 
-        public override bool TileCollideStyle(Projectile projectile, ref int width, ref int height, ref bool fallThrough, ref Vector2 hitboxCenterFrac)
+        // Preventing recalling yoyos from dealing damage
+        public override bool? CanDamage(Projectile projectile)
         {
-            Player player = Main.player[projectile.owner];
-            YoyoModPlayer modPlayer = player.GetModPlayer<YoyoModPlayer>();
-
-            if ((modPlayer.shimmerBag || modPlayer.moonlordBag) && projectile.aiStyle == 99 && !projectile.counterweight && Main.myPlayer == projectile.owner) { return false; }
+            if (projectile.aiStyle == 99)
+            {
+                return projectile.ai[0] != -1;
+            }
 
             return true;
         }
 
+        public override bool TileCollideStyle(Projectile projectile, ref int width, ref int height, ref bool fallThrough, ref Vector2 hitboxCenterFrac)
+        {
+            return projectile.GetOwner().GetModPlayer<YoyoModPlayer>().phasingYoyos && projectile.IsYoyo();
+        }
     }
 }

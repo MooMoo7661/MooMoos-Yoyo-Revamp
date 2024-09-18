7bdfbaf72e4992fe26c3742555ca6c89c135a1cd
Moo Moo <obsidian1392@gmail.com>
2024-09-18 11:34:44 -0600

    Removal of redundant condition

--- Content/Conditions/CombinationsModConditions.cs
+++ Content/Conditions/CombinationsModConditions.cs
@@ -4,8 +4,6 @@ namespace CombinationsMod.Content.Conditions
 {
     public static class YoyoModConditions
     {
-        public static Condition MasterOrExpertMode = new Condition("Mods.CombinationsMod.Conditions.MasterOrExpertMode", () => Main.masterMode || Main.expertMode);
-
         public static Condition EaterOfWorldsOrBrain = new Condition("Mods.CombinationsMod.Conditions.EaterOfWorldsOrBrain", () => Condition.DownedEaterOfWorlds.IsMet() || Condition.DownedBrainOfCthulhu.IsMet());
     }
 }

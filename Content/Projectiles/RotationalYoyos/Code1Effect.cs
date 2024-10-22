7a3f38d7706fce4ec3185680a3250e3cd6c63ea6
Moo Moo <obsidian1392@gmail.com>
2024-10-22 16:56:53 -0600

    Revamp of Code1 ability

--- Content/Projectiles/RotationalYoyos/Code1Effect.cs
+++ Content/Projectiles/RotationalYoyos/Code1Effect.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using CombinationsMod.Content.Projectiles.Explosions;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using Terraria;
@@ -10,6 +11,9 @@ namespace CombinationsMod.Content.Projectiles.RotationalYoyos
 {
     public class Code1Effect : ModProjectile
     {
+        bool floating = false;
+        bool setSecondStats = false;
+        bool colliding = false;
 
         public override void SetStaticDefaults()
         {
@@ -37,13 +41,27 @@ namespace CombinationsMod.Content.Projectiles.RotationalYoyos
         bool growing = true;
         bool shrinking = false;
 
+        public override bool? CanHitNPC(NPC target) => (setSecondStats && !target.immortal) || !setSecondStats;
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Projectile.velocity = Vector2.Zero;
+            return false;
+        }
+
         public override void AI()
         {
             Player player = Main.player[Projectile.owner];
+            Projectile proj = Main.projectile[(int)Projectile.ai[1]];
+
+            if (((proj.ai[0] == -1 || floating) && proj.localAI[0] > 120) || setSecondStats)
+            {
+                SecondAI();
+                floating = true;
+                return;
+            }
 
             Projectile.rotation += 0.2f;
-
-            Projectile proj = Main.projectile[(int)Projectile.ai[1]];
 
             if (!proj.active || proj.owner != Projectile.owner || proj.aiStyle != 99)
             {
@@ -73,15 +91,12 @@ namespace CombinationsMod.Content.Projectiles.RotationalYoyos
                 shrinking = false;
             }
 
-
-
             Projectile.ai[0] += 1f;
 
             float posX = proj.Center.X - (int)(Math.Cos(rad) * distance) - Projectile.width / 2;
             float posY = proj.Center.Y - (int)(Math.Sin(rad) * distance) - Projectile.height / 2;
 
             Projectile.position = new Vector2(posX, posY);
-
 
             int dust = Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height,
             DustID.BlueTorch, Projectile.velocity.X * 0.5f, Projectile.velocity.Y * 0.5f, 0, default, 1f);
@@ -93,6 +108,62 @@ namespace CombinationsMod.Content.Projectiles.RotationalYoyos
                 _initialized = true;
             }
         }
+
+        public void SecondAI()
+        {
+            if (!setSecondStats)
+            {
+                Projectile.timeLeft = 240;
+                Projectile.penetrate = 1;
+                Projectile.tileCollide = true;
+                setSecondStats = true;
+            }
+
+            Projectile.velocity.Y += 0.2f;
+
+            //NPC HomingTarget = FindClosestNPC(500f);
+
+            //if (HomingTarget == null/* || !IsValidTarget(HomingTarget)*/)
+            //{
+            //    return;
+            //}
+
+            //float length = Projectile.DirectionTo(HomingTarget.Center).Length() * 3f;
+            //float targetAngle = Projectile.AngleTo(HomingTarget.Center);
+            //Projectile.velocity = Projectile.velocity.ToRotation().AngleTowards(targetAngle, MathHelper.ToRadians(3)).ToRotationVector2() * length;
+            //Projectile.rotation = Projectile.velocity.ToRotation();
+        }
+
+        public NPC FindClosestNPC(float maxDetectDistance)
+        {
+            NPC closestNPC = null;
+
+            float sqrMaxDetectDistance = maxDetectDistance * maxDetectDistance;
+
+            foreach (var target in Main.ActiveNPCs)
+            {
+                if (IsValidTarget(target))
+                {
+                    float sqrDistanceToTarget = Vector2.DistanceSquared(target.Center, Projectile.Center);
+
+                    if (sqrDistanceToTarget < sqrMaxDetectDistance)
+                    {
+                        sqrMaxDetectDistance = sqrDistanceToTarget;
+                        closestNPC = target;
+                    }
+                }
+            }
+
+            return closestNPC;
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            if (Projectile.owner == Main.myPlayer && setSecondStats)
+            Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Vector2.Zero, ModContent.ProjectileType<FireExplosion>(), (int)(Projectile.damage * 1.4f), 2f, Projectile.owner);
+        }
+
+        public bool IsValidTarget(NPC target) => target.CanBeChasedBy() && Collision.CanHit(Projectile.Center, 1, 1, target.position, target.width, target.height);
 
         public override bool PreDraw(ref Color lightColor)
         {

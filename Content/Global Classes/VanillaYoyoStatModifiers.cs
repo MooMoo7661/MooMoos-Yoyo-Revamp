82bd63c4daf9c936177c6d8b4b63b646e12e988d
Moo Moo <obsidian1392@gmail.com>
2024-10-22 16:53:32 -0600

    Item modifications for vanilla yoyos

---
+++ Content/Global Classes/VanillaYoyoStatModifiers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombinationsMod.Content.Global_Classes
+{
+    public class VanillaYoyoStatModifiers : GlobalItem
+    {
+        public override void SetDefaults(Item item)
+        {
+            int type = item.type;
+
+            switch (type)
+            {
+                case ItemID.JungleYoyo:
+                    item.damage = 16;
+                    item.rare = ItemRarityID.Blue;
+                    break;
+                case ItemID.CorruptYoyo:
+                    item.damage = 18;
+                    item.rare = ItemRarityID.Orange;
+                    break;
+                case ItemID.CrimsonYoyo:
+                    item.damage = 19;
+                    item.rare = ItemRarityID.Orange;
+                    break;
+            }
+        }
+    }
+}

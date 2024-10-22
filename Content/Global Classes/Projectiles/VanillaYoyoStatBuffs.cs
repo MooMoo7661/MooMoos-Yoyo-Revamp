04ce82b555257bd4463fe574c31110d284adba51
Moo Moo <obsidian1392@gmail.com>
2024-10-22 16:51:56 -0600

    Stat change class for vanilla yoyos

---
+++ Content/Global Classes/Projectiles/VanillaYoyoStatBuffs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombinationsMod.Content.Global_Classes.Projectiles
+{
+    public class VanillaStatBuffs : GlobalProjectile
+    {
+        public override void SetDefaults(Projectile proj)
+        {
+            int type = proj.type;
+            switch (proj.type)
+            {
+                case ProjectileID.Rally:
+                    ProjectileID.Sets.YoyosLifeTimeMultiplier[type] = 8f;
+                    break;
+                case ProjectileID.CorruptYoyo:
+                    ProjectileID.Sets.YoyosLifeTimeMultiplier[type] = 12f;
+                    ProjectileID.Sets.YoyosMaximumRange[type] = 220f;
+                    ProjectileID.Sets.YoyosTopSpeed[type] = 13;
+                    break;
+                case ProjectileID.CrimsonYoyo:
+                    ProjectileID.Sets.YoyosLifeTimeMultiplier[type] = 10f;
+                    ProjectileID.Sets.YoyosMaximumRange[type] = 230f;
+                    ProjectileID.Sets.YoyosTopSpeed[type] = 13.3f;
+                    break;
+                case ProjectileID.JungleYoyo:
+                    ProjectileID.Sets.YoyosLifeTimeMultiplier[type] = 9f;
+                    ProjectileID.Sets.YoyosTopSpeed[type] = 11;
+                    ProjectileID.Sets.YoyosMaximumRange[type] = 170;
+                    break;
+                case ProjectileID.Code1:
+                    ProjectileID.Sets.YoyosLifeTimeMultiplier[type] = 13f;
+                    break;
+
+            }
+        }
+    }
+}

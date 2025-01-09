e0c5f6a01e82c550b8ccfce5003030529e5b5374
Moo Moo <obsidian1392@gmail.com>
2025-01-09 09:36:43 -0700

    Code change because old seems to be worse?

--- Content/Global Classes/CombinationsModNPCModifications.cs
+++ Content/Global Classes/CombinationsModNPCModifications.cs
@@ -11,52 +11,50 @@ namespace CombinationsMod.GlobalClasses
         int timer = 60;
         public override void ModifyShop(NPCShop shop)
         {
+            if (shop.NpcType == NPCID.Cyborg)
+                shop.Add(ModContent.ItemType<NaniteString>(), Condition.DownedGolem);
+
             if (shop.NpcType == NPCID.SkeletonMerchant)
             {
-                List<int> shopToRemove = new List<int>
-                {
-                    ItemID.YoYoGlove,
-                    ItemID.Gradient,
-                    ItemID.FormatC,
-                };
-
-                foreach(int item in shopToRemove)
-                {
-                    if (shop.TryGetEntry(item, out var entry))
-                        entry.Disable();
-                }
+                if (shop.TryGetEntry(ItemID.YoYoGlove, out var entry1))
+                    entry1.Disable();
+                
+                if (shop.TryGetEntry(ItemID.Gradient, out var entry2))
+                    entry2.Disable();
+                
+                if (shop.TryGetEntry(ItemID.FormatC, out var entry3))
+                    entry3.Disable();
 
                 for (int i = ItemID.BlackCounterweight; i <= ItemID.YellowCounterweight; i++)
                 {
                     if (shop.TryGetEntry(i, out var entry))
-                    {
                         entry.Disable();
-                    }
                 }
             }
-
-            if (shop.NpcType == NPCID.Cyborg)
-                shop.Add(ModContent.ItemType<NaniteString>(), Condition.DownedGolem);
         }
 
         public override void PostAI(NPC npc)
         {
-            if (npc.HasBuff(ModContent.BuffType<RootedDebuff>()) && Main.netMode != NetmodeID.MultiplayerClient)
+            if (!npc.HasBuff(ModContent.BuffType<RootedDebuff>()) || Main.netMode == NetmodeID.MultiplayerClient)
             {
-                timer--;
-                if (timer <= 0)
-                {
-                    var dir = Vector2.One.RotatedByRandom(MathHelper.TwoPi) * 8f;
-                    var proj = Projectile.NewProjectileDirect(Projectile.GetSource_NaturalSpawn(), npc.Center, Vector2.Zero, ProjectileID.Seed, 12, 0f);
-                    proj.usesLocalNPCImmunity = false;
-                    proj.localNPCHitCooldown = 10;
-                    proj.timeLeft = 12;
-                    proj.scale = 0.01f;
-                    timer = 30;
-                }
-            }
-            else
                 timer = 60;
+                return;
+            }
+
+            timer--;
+            
+            if (timer > 0)
+            {
+                return;
+            }
+
+            var proj = Projectile.NewProjectileDirect(Projectile.GetSource_NaturalSpawn(), npc.Center, Vector2.Zero, ProjectileID.Seed, 12, 0f);
+            proj.usesLocalNPCImmunity = false;
+            proj.localNPCHitCooldown = 10;
+            proj.timeLeft = 12;
+            proj.scale = 0.01f;
+
+            timer = 30;
         }
     }
 }

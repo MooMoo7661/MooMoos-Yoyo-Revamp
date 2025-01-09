a4140a4d2bea24879c92c5f00139b3b59b5fc139
Moo Moo <obsidian1392@gmail.com>
2025-01-09 09:31:02 -0700

    Swarm Spinner, Holiday Delight, and Pumpkin Patcher abiltiy rework + asset renames

---
+++ Content/Items/Yoyos/HolidayDelight.cs
@@ -0,0 +1,44 @@
+using CombinationsMod.Content.Configs;
+using CombinationsMod.Content.Projectiles.YoyoProjectiles;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CombinationsMod.Content.Items.Yoyos;
+
+[LegacyName("ChristmasBulb")]
+public class HolidayDelight : ModYoyo
+{
+    public override bool CanBeUnloaded => true;
+
+    public override void SetStaticDefaults()
+    {
+        ItemID.Sets.Yoyo[Item.type] = true;
+    }
+
+    public override void SetDefaults()
+    {
+        Item.useStyle = ItemUseStyleID.Shoot;
+        Item.width = 32;
+        Item.height = 32;
+        Item.useAnimation = 25;
+        Item.useTime = 25;
+        Item.shootSpeed = 23f;
+        Item.knockBack = 2.5f;
+        Item.damage = 78;
+        Item.rare = ItemRarityID.Yellow;
+        Item.DamageType = DamageClass.MeleeNoSpeed;
+        Item.channel = true;
+        Item.noMelee = true;
+        Item.noUseGraphic = true;
+        Item.UseSound = new SoundStyle?(SoundID.Item1);
+        Item.value = Item.sellPrice(0, 7, 8, 3);
+        Item.shoot = ModContent.ProjectileType<HolidayDelightProjectile>();
+    }
+
+    public override bool IsLoadingEnabled(Mod mod)
+    {
+        return ModContent.GetInstance<YoyoModConfig>().LoadModdedYoyos;
+    }
+}

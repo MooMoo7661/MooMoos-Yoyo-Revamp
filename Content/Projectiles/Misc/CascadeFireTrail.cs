f33f97e7f846c293256562d21a6bd04c10727567
Moo Moo <obsidian1392@gmail.com>
2025-01-09 09:50:22 -0700

    Additions of extra projectiles for new abilities

---
+++ Content/Projectiles/Misc/CascadeFireTrail.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombinationsMod.Content.Projectiles.Misc
+{
+    public class CascadeFireTrail : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 10;
+            Projectile.height = 10;
+            Projectile.damage = 20;
+            Projectile.DamageType = DamageClass.MeleeNoSpeed;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.usesIDStaticNPCImmunity = true;
+            Projectile.idStaticNPCHitCooldown = 20;
+            Projectile.aiStyle = -1;
+        }
+
+        public override bool? CanHitNPC(NPC target)
+        {
+            return false;
+        }
+
+        public override void PostAI()
+        {
+            Dust fire = Dust.NewDustDirect(Projectile.position, 3, 3, DustID.Torch, 0, 3f);
+            fire.noGravity = true;
+            fire.scale = 1.6f;
+        }
+    }
+}
---
+++ Content/Projectiles/Misc/PalladiumFist.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombinationsMod.Content.Projectiles.Misc
+{
+    public class PalladiumFist : ModProjectile
+    {
+        int timer = 0;
+        public override void SetDefaults()
+        {
+            Projectile.width = 30;
+            Projectile.height = 38;
+            Projectile.damage = 40;
+            Projectile.DamageType = DamageClass.MeleeNoSpeed;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.usesIDStaticNPCImmunity = true;
+            Projectile.idStaticNPCHitCooldown = 20;
+            Projectile.aiStyle = -1;
+            Projectile.penetrate = -1;
+        }
+
+        public override void PostAI()
+        {
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+
+            timer++;
+
+            if (timer >= 20)
+            {
+                if (Projectile.alpha < 255)
+                    Projectile.alpha += 4;
+                else
+                    Projectile.Kill();
+            }
+
+            Projectile.velocity.X *= 0.95f;
+        }
+
+        public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough, ref Vector2 hitboxCenterFrac)
+        {
+            return false;
+        }
+    }
+}

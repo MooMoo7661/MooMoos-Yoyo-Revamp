1749baefc889a8d599925aa634690a320e1c4a3b
Moo Moo <obsidian1392@gmail.com>
2026-01-27 11:22:39 -0700

    forgot to include this in last commit

--- Content/YoyoModItemLoader.cs
+++ Content/YoyoModItemLoader.cs
@@ -1,88 +1,23 @@
-﻿using CombinationsMod.Content.Configs;
+﻿using System.Globalization;
+using CombinationsMod.Content.Configs;
 using Terraria.ModLoader;
 
 namespace CombinationsMod.Content
 {
-    /// <summary>
-    /// This is to make it so the user can choose whether or not to include every item, using the Mod Config. Returns `LoadModdedItems` bool from YoyoModConfig.
-    /// </summary>
-    /// Renamed to YoyoModItemLoader to avoid confusion with terraria's ItemLoader
-    public abstract class YoyoModItemLoader : ModItem
+    public abstract class ModDrill : ModItem
     {
-        public override bool IsLoadingEnabled(Mod mod)
-        {
-            return ModContent.GetInstance<YoyoModConfig>().LoadModdedItems;
-        }
-    }
-
-    public abstract class ModDrill : YoyoModItemLoader
-    {
-        [CloneByReference]
-        public abstract bool CanBeUnloaded { get; }
         [CloneByReference]
         public abstract int DrillProjectile { get; }
-
-        public override bool IsLoadingEnabled(Mod mod)
-        {
-            if (!ModContent.GetInstance<YoyoModConfig>().LoadModdedAccessories)
-                return false;
-
-            if (!CanBeUnloaded)
-                return true;
-
-            return ModContent.GetInstance<YoyoModConfig>().LoadModdedItems;
-        }
     }
 
-    public abstract class ModRing : YoyoModItemLoader
+    public interface IYoyoUpgrade
     {
-        [CloneByReference]
-        public abstract bool CanBeUnloaded { get; }
+        LocalizedText Description { get; }
 
-        public override bool IsLoadingEnabled(Mod mod)
-        {
-            if (!ModContent.GetInstance<YoyoModConfig>().LoadModdedAccessories)
-                return false;
+        void ApplyEffects(Projectile proj) { }
 
-            if (!CanBeUnloaded)
-                return true;
+        void ApplyOnHitEffect(Projectile projectile, NPC target, NPC.HitInfo hit, int damageDone) { }
 
-            return ModContent.GetInstance<YoyoModConfig>().LoadModdedItems;
-        }
-    }
-
-
-    public abstract class ModString : YoyoModItemLoader
-    {
-        [CloneByReference]
-        public abstract bool CanBeUnloaded { get; }
-
-        public override bool IsLoadingEnabled(Mod mod)
-        {
-            if (!ModContent.GetInstance<YoyoModConfig>().LoadModdedAccessories)
-                return false;
-
-            if (!CanBeUnloaded)
-                return true;
-
-            return ModContent.GetInstance<YoyoModConfig>().LoadModdedItems;
-        }
-    }
-
-    public abstract class ModYoyo : YoyoModItemLoader
-    {
-        [CloneByReference]
-        public abstract bool CanBeUnloaded { get; }
-
-        public override bool IsLoadingEnabled(Mod mod)
-        {
-            if (!ModContent.GetInstance<YoyoModConfig>().LoadModdedYoyos)
-                return false;
-
-            if (!CanBeUnloaded)
-                return true;
-
-            return ModContent.GetInstance<YoyoModConfig>().LoadModdedItems;
-        }
+        void AI(Projectile proj) { }
     }
 }

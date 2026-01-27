1c214b0cc9d0e4b4559a9399552c301471cbbade
Moo Moo <obsidian1392@gmail.com>
2026-01-27 10:53:30 -0700

    WIP cal rebalancing

--- Content/Global Classes/Projectiles/VanillaYoyoStatBuffs.cs
+++ Content/Global Classes/Projectiles/VanillaYoyoStatBuffs.cs
@@ -8,6 +8,20 @@ namespace CombinationsMod.Content.Global_Classes.Projectiles
 {
     public class VanillaStatBuffs : GlobalProjectile
     {
+        public override bool AppliesToEntity(Projectile entity, bool lateInstantiation) => entity.aiStyle == 99;        
+
+        public override void SetDefaults(Projectile proj)
+        {
+            if (!ModLoader.HasMod("CalamityMod") && proj.usesLocalNPCImmunity == false)
+            {
+                proj.usesLocalNPCImmunity = true;
+                proj.localNPCHitCooldown = (10 + (proj.YoyoData().MainYoyo ? 0 : 10)) * (proj.extraUpdates + 1);
+
+                proj.usesIDStaticNPCImmunity = false;
+                proj.idStaticNPCHitCooldown = 0;
+            }
+        }
+
         public override void SetStaticDefaults()
         {
             ProjectileID.Sets.YoyosLifeTimeMultiplier[ProjectileID.Rally] = 8f;

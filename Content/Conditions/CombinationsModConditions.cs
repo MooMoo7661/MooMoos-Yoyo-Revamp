f15404c56f784a745048f8564137b8063fcf5b12
Moo Moo <obsidian1392@gmail.com>
2026-07-08 22:12:31 -0600

    Added new moon conditions for Yoyo Merchant

--- Content/Conditions/CombinationsModConditions.cs
+++ Content/Conditions/CombinationsModConditions.cs
@@ -6,6 +6,9 @@ namespace CombinationsMod.Content.Conditions
     {
         public static Condition EaterOfWorldsOrBrain = new("Mods.CombinationsMod.Conditions.EaterOfWorldsOrBrain", () => Condition.DownedEaterOfWorlds.IsMet() || Condition.DownedBrainOfCthulhu.IsMet());
         public static Condition NPCNotShimmered = new("Mods.CombinationsMod.Conditions.NPCNotShimmered", () => !Condition.IsNpcShimmered.IsMet());
-       
+        public static Condition MoonFullOrNew = new("Mods.CombinationsMod.Conditions.MoonFullOrNew", () => Condition.MoonPhaseFull.IsMet() || Condition.MoonPhaseNew.IsMet());
+        public static Condition MoonWaningGibbousOrWaxingCrescent = new("Mods.CombinationsMod.Conditions.MoonWaningGibbousOrWaxingCrescent", () => Condition.MoonPhaseWaningGibbous.IsMet() || Condition.MoonPhaseWaxingCrescent.IsMet());
+        public static Condition MoonThirdQuarterOrFirstQuarter = new("Mods.CombinationsMod.Conditions.MoonThirdQuarterOrFirstQuarter", () => Condition.MoonPhaseFirstQuarter.IsMet() || Condition.MoonPhaseThirdQuarter.IsMet());
+        public static Condition MoonWaningCrescentOrWaxingGibbous = new("Mods.CombinationsMod.Conditions.MoonWaningCrescentOrWaxingGibbous", () => Condition.MoonPhaseWaningCrescent.IsMet() || Condition.MoonPhaseWaxingGibbous.IsMet());
     }
 }

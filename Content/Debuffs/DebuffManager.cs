25b18325e177f7b0156f7747cd840d03b1e8467e
Moo Moo <obsidian1392@gmail.com>
2026-07-08 22:12:55 -0600

    Debuff manager for more debuff stuff

---
+++ Content/Debuffs/DebuffManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombinationsMod.Content.Debuffs
+{
+   public class DebuffManager : GlobalNPC
+   {
+        public override bool InstancePerEntity => true;
+
+        public override void UpdateLifeRegen(NPC npc, ref int damage)
+        {
+            if (npc.HasBuff(ModContent.BuffType<RedBearingDebuff>()))
+            {
+                if (npc.lifeRegen > 0)
+                {
+                    npc.lifeRegen = 0;
+                }
+
+                npc.lifeRegen -= 9;
+                damage = 3;
+            }
+        }
+
+   }
+}
---
+++ Content/Debuffs/RedBearingDebuff.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+
+namespace CombinationsMod.Content.Debuffs
+{
+    public class RedBearingDebuff : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.debuff[Type] = true;
+        }
+
+        public override void Update(NPC npc, ref int buffIndex)
+        {
+            if (Main.rand.NextBool(2))
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (Main.rand.NextBool(4))
+                        continue;
+
+                    var vel = Main.rand.NextVector2CircularEdge(1f, 1f);
+                    var dust = Dust.NewDustDirect(npc.position, npc.width, npc.height, DustID.Blood, vel.X, vel.Y);
+                    dust.scale = 0.8f + Main.rand.NextFloat(-0.4f, 0.3f);
+                }
+            }
+        }
+    }
+}
